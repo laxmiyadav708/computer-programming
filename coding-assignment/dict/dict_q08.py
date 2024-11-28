3dba6467254f53bfe9b00ac311032613b85b63e0
laxmiyadav708 <laxmiyadav9482@gmail.com>
2024-11-28 10:33:34 +0530

    dict_q08.py

---
+++ coding-assignment/dict/dict_q08.py
@@ -0,0 +1,3 @@
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+sorted_dict = dict(sorted(my_dict.items()))
+print(sorted_dict)  # Output: {'age': 25, 'city': 'New York', 'name': 'Alice'}
